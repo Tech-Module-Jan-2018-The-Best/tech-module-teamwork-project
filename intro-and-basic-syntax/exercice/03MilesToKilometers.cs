4fcafb3e722c5e4d8f60ff40358265cc85b0f331
amavrov <alexandermavrov@mail.bg>
2018-03-09 15:25:44 +0200

    added comments to problem 03

--- intro-and-basic-syntax/exercice/03MilesToKilometers.cs
+++ intro-and-basic-syntax/exercice/03MilesToKilometers.cs
@@ -6,10 +6,11 @@ namespace ConsoleApp7
     {
         static void Main(string[] args)
         {
+            // Here we read the amount of miles to be converted as a decimal data type from the console
             decimal miles = decimal.Parse(Console.ReadLine());
-
+            // here we convert the miles to kilometers (by the formula) and assign the value to a new variable
             decimal kilometers = miles * (decimal)1.60934;
-
+            // Finally we print the value of the kilometers with rouding to 2 numbers after the decimal point
             Console.WriteLine($"{kilometers:f2}");
 
         }
