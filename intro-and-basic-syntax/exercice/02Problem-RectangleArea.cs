c110fe63c7b1e8205ca74880d784d88c37b61ca1
Hristomir Asenov <hristomir.asenov@gmail.com>
2018-03-09 16:23:38 +0200

    Member 1 documents the code of Solution of Problem 2

--- intro-and-basic-syntax/exercice/02Problem-RectangleArea.cs
+++ intro-and-basic-syntax/exercice/02Problem-RectangleArea.cs
@@ -6,11 +6,18 @@ namespace Problem_02._Rectangle_Area
     {
         static void Main(string[] args)
         {
+			// Create a variable "width" from double data type.
+			// Parsing the input to double and save it to width.
             double width = double.Parse(Console.ReadLine());
+			
+			// Create a variable "height" from double data type.
+			// Parsing the input to double and save it to height.
             double height = double.Parse(Console.ReadLine());
 
+			// Create a variable "area" from double data type, which will save the result.
             double area = width * height;
 
+			// Print the area to the console.
             Console.WriteLine($"{area:f2}");
         }
     }
