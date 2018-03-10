b83a1b07045bc3f707497ff9a80a36023ad4d5a5
yoanaangelova <yoana_a@mail.bg>
2018-03-10 20:54:16 +0200

    Adding comments on exercise 01.DebitCardNumber

--- intro-and-basic-syntax/exercice/01Problem-DebitCardNumber.cs
+++ intro-and-basic-syntax/exercice/01Problem-DebitCardNumber.cs
@@ -6,11 +6,12 @@ namespace DebitCardNumber
     class DebitCardNumber
     {
         static void Main(string[] args)
-        {
+        {   //Here we use a for-cycle to receive the input and later to print it
             for (int i = 0; i < 4; i++)
             {
+                //Here we receive the input from the console and parse it to integer
                 int currentNum = int.Parse(Console.ReadLine());
-
+                //Here we print on the console the input in 4-digit debit card format
                 Console.Write("{000:0000} ", currentNum);
             }
             Console.WriteLine();
