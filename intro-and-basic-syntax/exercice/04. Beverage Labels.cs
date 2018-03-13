0b2e20ed8aacf5b9e8001e36507c12ab8755957e
AlexanderVlaskovski <a.vlaskovski@gmail.com>
2018-03-13 07:39:43 +0200

    Added comments on solution 04

--- intro-and-basic-syntax/exercice/04. Beverage Labels.cs
+++ intro-and-basic-syntax/exercice/04. Beverage Labels.cs
@@ -6,15 +6,22 @@ namespace _04._Beverage_Labels
     {
         static void Main(string[] args)
         {
+			//Here we recieve the product's name
             var name = Console.ReadLine();
+			//Here we recieve the product's volume
             int volume = int.Parse(Console.ReadLine());
+			//Here we recieve the energy content of the product per 100ml
             int energy = int.Parse(Console.ReadLine());
+			//Here we recieve the sugar content of the product per 100ml
             int sugar = int.Parse(Console.ReadLine());
 
-            double energy_need = (double)energy * volume / 100;
+			//On the next two lines we calculate the energy and the sugar content of the product
+            double energy_need = (double)energy * volume / 100;			
             double sugar_need = (double)sugar * volume / 100;
 
+			//Here we print the product's name and volume
             Console.WriteLine("{0}ml {1}:", volume, name);
+			//Here we print the energy and sugar content of the product
             Console.WriteLine("{0}kcal, {1}g sugars", energy_need, sugar_need);
         }
     }
