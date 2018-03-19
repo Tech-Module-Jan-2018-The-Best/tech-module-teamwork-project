4277b99c80b175d58a4cb1a9537be95780a84969
yoanaangelova <yoana_a@mail.bg>
2018-03-19 23:38:00 +0200

    Changing variable names in problem 03.Miles to Kilometres

--- intro-and-basic-syntax/exercice/03MilesToKilometers.cs
+++ intro-and-basic-syntax/exercice/03MilesToKilometers.cs
@@ -11,9 +11,9 @@ namespace ConsoleApp7
             // Here we read the amount of miles to be converted as a decimal data type from the console
             decimal initialMiles = decimal.Parse(Console.ReadLine()); // changing variable name from "miles" to "initianlMiles"
             // here we convert the miles to kilometers (by the formula) and assign the value to a new variable
-            decimal convertedKilometers = miles * (decimal)1.60934; // changing variable name from "kilometres" to "convertedKilometres"
+            decimal convertedKilometers = initialMiles * (decimal)1.60934; // changing variable name from "kilometres" to "convertedKilometres"
             // Finally we print the value of the kilometers with rouding to 2 numbers after the decimal point
-            Console.WriteLine($"{kilometers:f2}");
+            Console.WriteLine($"{convertedKilometers:f2}");
 
         }
     }
